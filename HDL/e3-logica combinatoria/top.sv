f63b480260ab2685ca0e15e07271445c6b28a8dd
Enock Bezerra Ferreira de Souza <enock.souza@ccc.ufcg.edu.br>
2021-12-22 12:24:04 -0300

    Ex 1.4 - Aeronave

--- HDL/e3-logica combinatoria/top.sv
+++ HDL/e3-logica combinatoria/top.sv
@@ -81,9 +81,17 @@ module top(input  logic clk_2,
   always_comb LED[2] <= sirene;
 
 
-  //Lógica do sistema (em lógica combinacional)
+  //Lógica do sistema (em estrutura condicional)
   always_comb
-    sirene <= (noite && paradas) || (sexta && producao && paradas);
+    /*sirene <= (noite && paradas) || (sexta && producao && paradas);*/
+
+    if (paradas)
+      if (noite) sirene <= 1;
+      else if (producao)
+        if (sexta) sirene <= 1;
+        else sirene <= 0;
+      else sirene <= 0;
+    else sirene <= 0;
 
   //Ou seja, o LED de indice 2 (da direita para a equerda no simulador) deverá ser ativado se, e somente se, ocorrer ao menos uma das seguintes combinações de switchs ativados: 4 e 5; ou 5, 6 e 7.
 
@@ -109,12 +117,13 @@ module top(input  logic clk_2,
   always_comb
     SEG[0] <= alarme;
 
-  //Lógica do sistema (com estrutura condicional)
+  //Lógica do sistema (hibrido com esturura condicional e lógica combinacional)
   always_comb
     if ( (porta_cofre && interruptor) ||
           (porta_cofre && !relogio) )
           alarme <= 1;
     else alarme <= 0;
+
 
   //Ou seja, o segmento 0 (alarme) será ativado se e somente se ao menos uma das seguintes combinações ocorrer: SWI[0] = 1 e SWI[2] = 1; ou SWI[0] = 1 e SWI[1] = 0.
 
@@ -142,11 +151,43 @@ module top(input  logic clk_2,
     SEG[7] <= inconsistencia;
   end
 
-  //Lógica do sistema
+  //Lógica do sistema (em lógica combinacional)
   always_comb begin
     aquecedor <= !t1 && !t2; //Se a temperatura estiver abaixo de 15 e 20 graus simultaneamente (SWI[3] = 0 e SWI[4] = 0) o aquecedor (LED[6]) será ativado.
     resfriador <= t1 && t2; //Se a temperatura estiver igual ou maior que 15 e 20 graus simultaneamente (SWI[3] = 1 e SWI[4] = 1) o resfriador (LED[7]) será ativado.
     inconsistencia <= !t1 && t2; //Se a temperatura estiver igual ou maior que 20 graus (SWI[4] = 1) e ao mesmo tempo estiver menor que 15 graus (SWI[3] = 0), então o indicador de insconsistência (SEG[7]) será ativado.
   end
+
+
+  //---------------------------------------------------------
+  // Exercício 4 - AERONAVE
+  //---------------------------------------------------------
+
+  //Declarando variáveis de entrada
+  logic trancaF, trancaM1, trancaM2;
+
+  //Declarando variáveis de saída
+  logic livreF, livreM;
+
+  //Conexões de entrada
+  always_comb begin
+    trancaF <= SWI[0];
+    trancaM1 <= SWI[1];
+    trancaM2 <= SWI[2];
+  end
+
+  //Conexões de saída
+  always_comb begin
+    LED[0] <= livreF;
+    LED[1] <= livreM;
+  end
+
+  //Lógica do sistema (em lógica combinacional)
+  always_comb begin
+    livreF <= !trancaF || !trancaM1 || !trancaM2; //Se e somente se a tranca de pelo menos um dos banheiros (seja exclusivamente feminino ou não) estiver desativada (SWI[0] = 0 ou SWI[1] = 0 ou SWI[2] = 0), então haverá lavatório feminino livre (LED[0] = 1).
+    livreM <= !trancaM1 || !trancaM2; //Se e somente se a tranca de pelo menos um dos dois banheiros masculinos estiver desativada (SWI[1] = 0 ou SWI[2] = 0), o lavatório masculino estará livre (LED[1] = 1).
+  end
+
+  //Obs: este exercício foi feito considerando que há um banheiro esclusivamente feminino, porém não um exclusivamente masculino
 
 endmodule
